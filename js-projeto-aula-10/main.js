3814b7a7268f093925d7c22dc466c7d676967d22
pinduca007 <130066449+pinduca007@users.noreply.github.com>
2024-09-25 10:17:26 -0300

    o=[io

--- js-projeto-aula-10/main.js
+++ js-projeto-aula-10/main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2012-01-30T00:00:00");
-const tempoObjetivo2 = new Date("2019-11-13T00:00:00");
-const tempoObjetivo3 = new Date("2023-09-26T10:10:00");
+const tempoObjetivo1 = new Date("2025-02-15T00:00:00");
+const tempoObjetivo2 = new Date("2030-04-24T00:00:00");
+const tempoObjetivo3 = new Date("2030-09-28T10:10:00");
 const tempoObjetivo4 = new Date("2024-09-25T10:10:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
